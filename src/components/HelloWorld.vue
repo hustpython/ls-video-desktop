2dfb3585c0eea6684f1169d6cdc29587e02f4088
mxq <yzwmxq@gmail.com>
2023-01-18 08:38:37 +0800

    格式调整

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,17 @@
 <script setup lang="ts">
     import {ref} from 'vue'
-
     const win: any = window;
     const platform = win.electronAPI.platform;
-
     const rspFromMainValue = ref();
     const ipcRenderOnCallBack = (_: any, data: any) => {
         rspFromMainValue.value = data.message;
     }
-
     win.electronAPI.rspToRender(ipcRenderOnCallBack);
-
     const sendToMain = () => {
         win.electronAPI.ipcRenderer.send('sendToMain', "来自渲染进程的消息");
     };
-
     defineProps<{ msg: string }>()
-
     const count = ref(0)
-
-
 </script>
 
 <template>
@@ -27,12 +19,10 @@
     <div class="card">
         <button type="button" @click="count++">count is {{ count }}</button>
     </div>
-
     <div>来自预加载脚本的数据 {{platform}}</div>
     <div class="card">
         <button type="button" @click="sendToMain">点击发送消息给主进程</button>
     </div>
-
     <div>来自electron主进程的消息: {{rspFromMainValue}}</div>
 </template>
 
